7dd0ce9838b433051558582b777f95d3abcc6748
ketan <ketanverma0001@gmail.com>
2024-11-14 12:29:51 +0530

    final commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const reviews = [
     {
       id: 1,
-      name: 'susan smith',
-      job: 'web developer',
-      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      name: 'Ketan Verma',
+      job: 'Backend developer',
+      img: './images/image.png',
       text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
       id: 2,
-      name: 'anna johnson',
+      name: 'Vishal Saini',
       job: 'web designer',
-      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      img: './images/image2.png',
       text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
     },
     {
       id: 3,
-      name: 'peter jones',
+      name: 'Gagan Bajwa',
       job: 'intern',
-      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      img: './images/image3.png',
       text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
     },
     {
       id: 4,
-      name: 'bill anderson',
-      job: 'the boss',
-      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      name: 'Raman',
+      job: 'Junior Developer',
+      img: './images/image4.png',
       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
   ];
@@ -64,9 +64,9 @@ const reviews = [
     showPersom(currentItem)
   })
   prevBtn.addEventListener('click',function(){
-    currentItem++
-    if(currentItem<0){
-        currentItem=reviews.length-1;
+    currentItem--
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
     }
     showPersom(currentItem)
   })
